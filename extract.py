2d8d7bce009339f0e5eeb9efe2258b599e256e95
yasohero11 <220170047@fa-hists.edu.eg>
2020-04-05 20:15:03 +0200

    update function

--- extract.py
+++ extract.py
@@ -1,4 +1,9 @@
 import csv
+import re
+
+myData =  []
+
+
 def getavg(cnum) :
     sum = 0
     count = 0
@@ -15,103 +20,169 @@ def setdata(tweetlenth) :
         writer.writerow(["null", "null", "null","null","null","null", "null", "null","null","null","null", "null", "null","null","null","null", "null", "null","null","null","null", "null", "null","null","AVG = "+str(tweetlenth),"null", "null", "null","null","null","null", "null", "null","null","null","null", "null", "null","null","null","null", "null", "null","null","null"])
 
 
-def insertRow(row, index, data):
-        row.insert(index,data)
-        
 
-def getUserDescriptionLength(fileName , newFileName , columns , columnsNames):
-        if(len(columns) != len(columnsNames)):
-            print("please enter same number of values of columns and columns_Names")
-            return
-        with open(fileName +'.csv','rt')as read_obj, \
-           open(newFileName + '.csv', 'a+', newline='') as write_obj:
-            data = csv.reader(read_obj)
-            csv_writer = csv.writer(write_obj)
+
+def getUserDescriptionLength(fileName , columns , columnsNames):
+        
+            
+            
+    
             count = 0
-            for row in data:
-                rowLength =  len(row)
+            for row in myData:
+               
                 if count != 0:
                     for i in range(len(columns)):                                                             
-                         row.insert(rowLength+i,len(row[int(columns[i])].replace(" ", "")))
-
-                    csv_writer.writerow(row)        
+                        row[columnsNames[i]] = len(row[columns[i]].replace(" ", ""))
+                       
+                     
                 else:
                     for i in range(len(columnsNames)):
-                        row.insert(rowLength+i,columnsNames[i])
-
-                    csv_writer.writerow(row)
+                        row[columnsNames[i]] =columnsNames[i]
                     count=1
 
+                 
 
                     
 
-def roundColuemns(fileName , newFileName , columns , columnsNames):
-        if(len(columns) != len(columnsNames)):
-            print("please enter same number of values of columns and columns_Names")
-            return
-        with open(fileName +'.csv','rt')as read_obj, \
-           open(newFileName + '.csv', 'a+', newline='') as write_obj:
-            data = csv.reader(read_obj)
-            csv_writer = csv.writer(write_obj)
+def roundColuemns(fileName , columns , columnsNames):
+           
+        
+           
             count = 0
-            for row in data:
-                rowLength = len(row)
+            for row in myData:
+                
                 if count != 0:
                     
                     for i in range(len(columns)): 
                         try: 
-                            cell = row[int(columns[i])]
+                            cell = row[columns[i]]
                             
                             if(cell != "?"):        
                                 
-                                row.insert(rowLength+i,round(float(cell)))
+                                row[columnsNames[i]] = round(float(cell))
                             else:
-                                row.insert(rowLength+i,"?") 
+                                row[columnsNames[i]] = "?" 
                         except Exception:
                                 print("error in 68")
                                                         
-                    csv_writer.writerow(row)                          
+                                            
                 else:
                     for i in range(len(columnsNames)):
-                        row.insert(rowLength+i,columnsNames[i])
-
-                    csv_writer.writerow(row)
+                        row[columnsNames[i]] = columnsNames[i]
+                
                     count=1
+            
 
-
-#                       6,7
-def getRatio(fileName , newFileName , columns , columnsNames):
+def getRatio(fileName , columns , columnsNames):
         
-        with open(fileName +'.csv','rt')as read_obj, \
-           open(newFileName + '.csv', 'a+', newline='') as write_obj:
-            data = csv.reader(read_obj)
-            csv_writer = csv.writer(write_obj)
-            count = 0
-            for row in data:
-                rowLength =  len(row)
-              
-                if count != 0:
-                    try:        
-                        num1 = int(row[int(columns[0])])
-                        num2 = int(row[int(columns[1])])                                                           
-                        row.insert(rowLength+1,(num1/num2))
-                    except Exception:
-                            row.insert(rowLength+1,0)
-                    csv_writer.writerow(row)        
-                else:
-                  
-                    row.insert(rowLength+1,columnsNames)
+            if(len(columns) > 2):
+                print("you can't enter more than two columens")
+                return
 
-                    csv_writer.writerow(row)
+            count = 0
+            for row in myData:
+                      
+                if count != 0:
+                    try:
+                        
+                         num1 = int(row[columns[0]])
+                         num2 = int(row[columns[1]])                                                                         
+                         row[columnsNames] = num1/num2
+                         
+                    except Exception as e:
+                           row[columnsNames] = 0                 
+                else:                  
+                    row[columnsNames] = columnsNames 
                     count=1
 
+def checkColuemns(coluemn1 , coluemn2):
+    if(len(coluemn1) ==  len(coluemn2)) :
+         return True 
+    else: 
+         return False
+              
+
+
+        
+
+        
+
+
+
+def readData(filePath):
+    try:
+        with open(filePath,'rt')as read_obj:
+                data = csv.DictReader(read_obj)                        
+                for row in data:                    
+                    myData.append(row)
+    except Exception as e:
+        print(e)
+
+       
+
+def writeData(filePath):
+    try:
+        with open( filePath, 'w', newline='') as write_obj:
+                    csv_writer = csv.writer(write_obj)  
+
+                    for row in myData:
+                        csv_writer.writerow(row.values())            
+
+                    
+    except Exception as e:
+        print(e)
                
 
 def main():
+    print()
     #setdata(getavg(24))
     #getUserDescriptionLength("Testing" , "t2" , ["3","37"],["userDis","userScreenNameLe"]) 
     #roundColuemns("Testing" , "t1" , ["33","34","35"],["round1","round2","round2"])
-    getRatio("Testing" , "t2" , ["6","7"],"raio")
+    #getRatio("Testing" , ["User.Followers_count","User.Friends_count"],"raio")
+
+    #   with open("Testing" +'.csv','rt')as read_obj:
+          
+    #         data = csv.DictReader(read_obj)
+    #         try:
+    #             for row in data:
+    #                 print(str(row["raio"]))
+    #         except Exception:
+    #             print("any")    
+    
+        # fileName =  input("please enter the file path here : ")
+        # newFile = input("do you want the results in other file? if yes please enter the path and the new file name with .csv at the end in no just press enter : ")
+        # if(newFile == ""):
+        #     newFile  = fileName
+
+
+        # answer = input("please enter your requiest : \n 1. for avrage enter 'a' \n 2. for charcters count enter 'c' \n 3. for round columens enter 'r' \n answer : ")
+        # if(len(answer) == 1):
+        #     if(re.findall("a|c|r", answer)):
+
+        #         if(answer == "c"):
+        #             columns = (input("enter columns names with sperated ',' : ")).replace(" " , "").split(",") 
+        #             columnsNames = (input("enter new columns names with sperated ',' : ")).replace(" " , "").split(",") 
+        #             if(checkColuemns(columns , columnsNames)):
+        #                 getUserDescriptionLength(fileName , columns , columnsNames)    
+
+                    
+        #             #print("the columens names not equal to new columens names, try agaien")
+      
+        # else:
+        #     print("you enterd a word not a charter, please try ")
+
+
+        # input("exit")     
+
+        #readData(fileName)
+        
+        #getUserDescriptionLength("training2" , ["User.Screen_name" , "User.Description"],["count_user", "dis_count"]) 
+        #getRatio("Testing" , ["User.Followers_count","User.Friends_count"],"raio")
+        #roundColuemns("Testing (1)" ,["FollowersToAccountAge" , "StatusesCountToAccountAge" , "raio"] , ["ratio raio" , "raio1", "rasio2"] )
+
+        #writeData(newFile)   
+    #getRatio("Testing" , ["User.Followers_count","User.Friends_count"],"raio")
+    
     
    
 
